6ae211e8429676540d2988abf1540723517b51bd
TemurEShtemirov <temureshtemirov10@gmail.com>
2025-01-15 19:45:00 +0500

    fix little bug

---
+++ .env
@@ -0,0 +1,2 @@
+BOT_TOKEN = 7386350363:AAEyYOL_Gxs7zaHYvfSpfYa-HEEEklg4QVQ
+API_URL = https://ishbazar-master-server.onrender.com
--- src/commands/start.js
+++
@@ -1,2 +0,0 @@
-BOT_TOKEN = 7386350363:AAEyYOL_Gxs7zaHYvfSpfYa-HEEEklg4QVQ
-API_URL = https://ishbazar-master-server.onrender.com
